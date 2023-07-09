fbd64b3de1a12598b015d0f0d41985a02d61e862
FalkoKa <115454465+FalkoKa@users.noreply.github.com>
2023-07-09 12:56:21 +0200

    mutation delete client and cache

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,9 +2,28 @@ import Header from './components/Header';
 import Clients from './components/Clients';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge({} = {}, incoming) {
+            return incoming;
+          },
+        },
+        projects: {
+          merge({} = {}, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
+
 const client = new ApolloClient({
   uri: 'http://localhost:3000/graphql',
-  cache: new InMemoryCache(),
+  cache: cache,
 });
 
 function App() {
--- client/src/components/ClientRow.tsx
+++ client/src/components/ClientRow.tsx
@@ -1,18 +1,40 @@
 import { Client } from './Clients';
 import { FaTrash } from 'react-icons/fa';
+import { useMutation } from '@apollo/client';
+import { DELETE_CLIENT } from '../mutations/clientMutations';
+import { GET_CLIENTS } from '../queries/clientQueries';
 
 type Props = {
   client: Client;
 };
 
+interface Cache {
+  [index: string]: any;
+}
+
 const ClientRow = ({ client }: Props) => {
+  const [deleteClient] = useMutation(DELETE_CLIENT, {
+    variables: { id: client.id },
+    update(cache: Cache, { data: { deleteClient } }) {
+      const { clients } = cache.readQuery({ query: GET_CLIENTS });
+      cache.writeQuery({
+        query: GET_CLIENTS,
+        data: {
+          clients: clients.filter(
+            (client: Client) => client.id !== deleteClient.id
+          ),
+        },
+      });
+    },
+  });
+
   return (
     <tr>
       <td>{client.name}</td>
       <td>{client.email}</td>
       <td>{client.phone}</td>
       <td>
-        <button>
+        <button onClick={() => deleteClient}>
           <FaTrash />
         </button>
       </td>
