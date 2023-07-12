2945da0abc51bebda78de90b0f301b9ed0f811d7
FalkoKa <115454465+FalkoKa@users.noreply.github.com>
2023-07-12 22:12:35 +0200

    add project components

--- client/src/pages/Project.tsx
+++ client/src/pages/Project.tsx
@@ -1,7 +1,33 @@
+import { Link, useParams } from 'react-router-dom';
+import Spinner from '../components/Spinner';
+import { useQuery } from '@apollo/client';
+import { GET_PROJECT } from '../queries/projectQueries';
+
 const Project = () => {
+  const { id } = useParams();
+  const { loading, error, data } = useQuery(GET_PROJECT, { variables: { id } });
+
+  if (loading) return <Spinner />;
+  if (error) return <p>Something went wrong...</p>;
+
   return (
     <>
-      <div>test</div>
+      <div>
+        {!loading && !error && (
+          <div>
+            <Link
+              className="mt-4 px-4 py-2 text-white bg-blue-600 hover:bg-blue-700 rounded-md"
+              to="/"
+            >
+              Back
+            </Link>
+            <h1>{data.project.name}</h1>
+            <p>{data.project.description}</p>
+            <h5>Project Status</h5>
+            <p>{data.project.status}</p>
+          </div>
+        )}
+      </div>
     </>
   );
 };
